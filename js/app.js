b2ccd8b8e14e931aebb49b228dee8e19e5a4dd2b
Kareem Mostafa <kareemmmostafaa@gmail.com>
2018-05-24 18:25:11 +0200

    Adding Enemy moving condition in Update function

--- js/app.js
+++ js/app.js
@@ -18,6 +18,11 @@ class Enemy {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if(this.x < 505) {
+      this.x = this.x + this.speed * dt;
+    }else{
+      this.x = -2;
+    }
 }
 
 // Draw the enemy on the screen, required method for game
